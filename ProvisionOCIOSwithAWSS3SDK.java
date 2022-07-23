5711b88f928ade9402a29bbc99a594e504d1d354
lxcao <caolingxin@hotmail.com>
2022-07-23 15:50:06 +0800

    path-based access

--- ProvisionOCIOSwithAWSS3SDK.java
+++ ProvisionOCIOSwithAWSS3SDK.java
@@ -1,11 +1,17 @@
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.List;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class ProvisionOCIOSwithAWSS3SDK {
     public static void main(String[] args) throws Exception {
@@ -19,6 +25,8 @@ public class ProvisionOCIOSwithAWSS3SDK {
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .withEndpointConfiguration(endpointConfigure)
+                .disableChunkedEncoding()
+                .enablePathStyleAccess()
                 .build();
 
         System.out.print("****************************listBuckets****************************");
@@ -28,5 +36,32 @@ public class ProvisionOCIOSwithAWSS3SDK {
             System.out.println("* " + b.getName());
         }
 
+        System.out.println("****************************putObject****************************");
+        String file_path = "/Users/caolingxin/Documents/workspaces/oci-projects/oci-os-s3-compatibility/assets/wholesale-trade-survey-mar-2022-quarter-csv.csv";
+        String bucket_name = "bucket-20220722";
+        String key_name = Paths.get(file_path).getFileName().toString()+"10";
+
+        System.out.format("Uploading %s to OCI bucket %s...\n", key_name, bucket_name);
+        try {
+            s3Client.putObject(bucket_name, key_name, new File(file_path));
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("Done!");
+
+        System.out.println("****************************listObjects****************************");
+        ObjectListing result = s3Client.listObjects(bucket_name);
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+        for (S3ObjectSummary os : objects) {
+            System.out.println("* " + os.getKey());
+        }
+
+        System.out.println("****************************listObjectsV2****************************");
+        ListObjectsV2Result resultv2 = s3Client.listObjectsV2(bucket_name);
+        List<S3ObjectSummary> objectsv2 = resultv2.getObjectSummaries();
+        for (S3ObjectSummary os : objectsv2) {
+            System.out.println("* " + os.getKey());
+        }
     }
 }
