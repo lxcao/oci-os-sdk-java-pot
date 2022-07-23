cd80590a0a3a79f9f9a7bfd0958c1d80e1c15b33
lxcao <caolingxin@hotmail.com>
2022-07-23 16:21:23 +0800

    get

--- ProvisionOCIOSwithAWSS3SDK.java
+++ ProvisionOCIOSwithAWSS3SDK.java
@@ -1,6 +1,10 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -11,6 +15,9 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class ProvisionOCIOSwithAWSS3SDK {
@@ -39,7 +46,7 @@ public class ProvisionOCIOSwithAWSS3SDK {
         System.out.println("****************************putObject****************************");
         String file_path = "/Users/caolingxin/Documents/workspaces/oci-projects/oci-os-s3-compatibility/assets/wholesale-trade-survey-mar-2022-quarter-csv.csv";
         String bucket_name = "bucket-20220722";
-        String key_name = Paths.get(file_path).getFileName().toString()+"10";
+        String key_name = Paths.get(file_path).getFileName().toString() + UUID.randomUUID();
 
         System.out.format("Uploading %s to OCI bucket %s...\n", key_name, bucket_name);
         try {
@@ -62,6 +69,41 @@ public class ProvisionOCIOSwithAWSS3SDK {
         List<S3ObjectSummary> objectsv2 = resultv2.getObjectSummaries();
         for (S3ObjectSummary os : objectsv2) {
             System.out.println("* " + os.getKey());
+
+        System.out.println("****************************getObject****************************");
+        System.out.format("Downloading %s from S3 bucket %s...\n", key_name, bucket_name);
+        try {
+            S3Object o = s3Client.getObject(bucket_name, key_name);
+            S3ObjectInputStream s3is = o.getObjectContent();
+                FileOutputStream fos = new FileOutputStream(new File(key_name));
+                byte[] read_buf = new byte[1024];
+                int read_len = 0;
+                while ((read_len = s3is.read(read_buf)) > 0) {
+                    fos.write(read_buf, 0, read_len);
+                }
+                s3is.close();
+                fos.close();
+            } catch (AmazonServiceException e) {
+                System.err.println(e.getErrorMessage());
+                System.exit(1);
+            } catch (FileNotFoundException e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
+            System.out.println("Done!");
         }
+
+        System.out.println("****************************getObjectAsString****************************");
+        String oString = s3Client.getObjectAsString(bucket_name, key_name);
+        System.out.format("length of downloaded object is %s\n",oString.length());
+        System.out.println("Done!");
+
+        System.out.println("****************************getObjectMetadata****************************");
+        ObjectMetadata oMetadata = s3Client.getObjectMetadata(bucket_name, key_name);
+        System.out.format("length of Content is %s\n",oMetadata.getContentLength());
+        System.out.println("Done!");
     }
 }
